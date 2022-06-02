a3cdcd7a962d113ff7d4a8f7f85199d33a0d56ce
Egor <75725159+ennse1@users.noreply.github.com>
2022-06-02 18:13:18 +0700

    Update RomanNumberTests.cs

--- RomanTests/RomanNumberTests.cs
+++ RomanTests/RomanNumberTests.cs
@@ -15,16 +15,13 @@ namespace RomanNumbers.Tests
         [TestMethod()]
         public void Add()
         {
-            //arrange
             var romanNumber = new RomanNumber(8);
             var romanNumber1 = new RomanNumber(1);
             var romanNumber2 = new RomanNumber(120);
             var expected1 = new RomanNumber(9);
             var expected2 = new RomanNumber(128);
-            //act
             RomanNumber actual1 = romanNumber + romanNumber1;
             RomanNumber actual2 = romanNumber + romanNumber2;
-            //assert
             Assert.AreEqual(expected1.ToString(), actual1.ToString());
             Assert.AreEqual(expected2.ToString(), actual2.ToString());
 
@@ -32,17 +29,13 @@ namespace RomanNumbers.Tests
         [TestMethod()]
         public void Sub()
         {
-            //arrange
             var romanNumber = new RomanNumber(10);
             var romanNumber1 = new RomanNumber(1);
             var romanNumber2 = new RomanNumber(120);
             var expected = new RomanNumber(9);
 
-            //act
-
             RomanNumber actual = romanNumber - romanNumber1;
 
-            //assert
             Assert.AreEqual(expected.ToString(), actual.ToString());
             Assert.ThrowsException<RomanNumberException>(() => (romanNumber - romanNumber2));
 
@@ -55,27 +48,21 @@ namespace RomanNumbers.Tests
             var romanNumber2 = new RomanNumber(120);
             var expected = new RomanNumber(5);
 
-            //act
-
             RomanNumber actual = romanNumber / romanNumber1;
 
-            //assert
             Assert.AreEqual(expected.ToString(), actual.ToString());
             Assert.ThrowsException<RomanNumberException>(() => (romanNumber / romanNumber2));
         }
         [TestMethod()]
         public void Mul()
         {
-            //arrange
             var romanNumber = new RomanNumber(8);
             var romanNumber1 = new RomanNumber(6);
             var romanNumber2 = new RomanNumber(12);
             var expected1 = new RomanNumber(48);
             var expected2 = new RomanNumber(96);
-            //act
             RomanNumber actual1 = romanNumber * romanNumber1;
             RomanNumber actual2 = romanNumber * romanNumber2;
-            //assert
             Assert.AreEqual(expected1.ToString(), actual1.ToString());
             Assert.AreEqual(expected2.ToString(), actual2.ToString());
         }
@@ -83,24 +70,18 @@ namespace RomanNumbers.Tests
         [TestMethod()]
         public void ToStringTest()
         {
-            //arrange
             RomanNumber romanNumber = new RomanNumber(1002);
             string expected = "MII";
-            //act
             string actual = romanNumber.ToString();
-            //assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
         public void CloneTest()
         {
-            //arrange
             RomanNumber romanNumber = new RomanNumber(5);
             RomanNumber expected = new RomanNumber(5);
-            //act
             RomanNumber clone = (RomanNumber)romanNumber.Clone();
-            //assert
             Assert.AreEqual(expected.ToString(), clone.ToString());
             Assert.AreNotSame(expected, clone);
         }
@@ -109,13 +90,10 @@ namespace RomanNumbers.Tests
         [TestMethod()]
         public void CompareToTest()
         {
-            //arrange
             RomanNumber romanNumber = new RomanNumber(10);
             RomanNumber romanNumber1 = new RomanNumber(10);
             const int expected = 0;
-            //act
             int actual = romanNumber.CompareTo(romanNumber1);
-            //assert
             Assert.AreEqual(expected, actual);
         }
     }
